42e99902bac2bebd880d1c9b235f3a9d49677d16
Mai Lan Peng <mailan.peng@gmail.com>
2018-08-06 20:42:04 +0200

    first commit

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(first_name: "Example User", last_name: "gzprgkr", password:'azerty', email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "first name should be present" do
+    @user.first_name = ''
+    assert not(@user.valid?)
+  end
+
+  test "first name should not contain space" do
+    @user.first_name = ''
+    assert not(@user.valid?)
+  end
+
+  test "should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert not(duplicate_user.valid?)
+  end
+
 end
