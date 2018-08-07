4fe37722fde484866724e4a149a20e53db6fc289
Mai Lan Peng <mailan.peng@gmail.com>
2018-08-07 04:47:23 +0200

    f last push before 6.AM

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do
+    get root_path
     assert_response :success
   end
-
-  test "should get secret" do
-    get static_pages_secret_url
-    assert_response :success
-  end
-
 end
